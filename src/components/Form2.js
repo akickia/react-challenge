230aaf5c68731da62c3d124f4d3f5e8b70d2c379
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2022-06-18 12:11:43 +0200

    test

--- src/components/Form2.js
+++ src/components/Form2.js
@@ -1,47 +1,18 @@
 import { useState } from "react";
 
 const Form2 = () => {
-  const [inputData, setInputData] = useState({
-    YourName: "",
-    Mail: "",
-    message: "",
-  });
-
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setInputData ({...inputData, [name]: value})
-  }
-
-  const [userInput, setUserInput] = useState([]);
-    
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const newInput = {
-      ...inputData,
-    }
-
-    setUserInput ([...userInput, newInput])
-    
-    setInputData ({
-      YourName: "",
-      Mail: "",
-      message: "",
-    })
-   }
-
+  
 
   
   return (
   <>
 
-  <form name="contact" method="POST" onSubmit={handleSubmit}>
+  <form name="contact" method="POST">
   <input type="hidden" name="form-name" value="contact" />
-    <input onChange={handleChange} type="text" id="Yourname" name="YourName" value={inputData.YourName} placeholder="Your Name" />
-    <input onChange={handleChange} type="email" id="Mail" name="Mail" value={inputData.Mail} placeholder="Your Email" />
-    <input onChange={handleChange} type="text" name="message" id="message" value={inputData.message} placeholder="Your message" />
-    <button className="form-btn btn" type="submit" formMethod="POST">SEND MESSAGE</button> 
+    <input type="text" id="Yourname" name="YourName" placeholder="Your Name" />
+    <input type="email" id="Mail" name="Mail" placeholder="Your Email" />
+    <input type="text" name="message" id="message"  placeholder="Your message" />
+    <button className="form-btn btn" type="submit" >SEND MESSAGE</button> 
          
   </form>
   
