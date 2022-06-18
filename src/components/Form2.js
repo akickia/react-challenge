b017d3f0cb66d9ab6cf3ffc14bf472b11dc77c06
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2022-06-18 11:45:15 +0200

    form validation

--- src/components/Form2.js
+++ src/components/Form2.js
@@ -36,7 +36,7 @@ const Form2 = () => {
   return (
   <>
 
-  <form onSubmit={handleSubmit} name="contact" data-netlify="true" method="POST" >
+  <form onSubmit={handleSubmit} method="POST" >
   <input type="hidden" name="form-name" value="contact" />
     <input onChange={handleChange} type="text" id="Yourname" name="YourName" value={inputData.YourName} placeholder="Your Name" />
     <input onChange={handleChange} type="email" id="Mail" name="Mail" value={inputData.Mail} placeholder="Your Email" />
